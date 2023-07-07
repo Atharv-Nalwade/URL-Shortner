1b83c2d997d03e64b6112648133e24f6263dbfb9
Atharv-Nalwade <atharv.n2002@gmail.com>
2023-07-07 23:41:27 +0530

    WIP: Work in progress - Redis caching implementation

--- src/repository/url-repository.js
+++ src/repository/url-repository.js
@@ -1,22 +1,73 @@
 const URL = require("../models/url");
+const redisClient = require("../configs/redisconfig");
+
+
 
 class UrlRepository {
+  // async getUrl(data) {
+  //   try {
+  //     console.log("Dats is",data);
+  //     const answer = await redisClient.get(data);
+  //     console.log(answer);
+  //     if(answer){
+  //       console.log("hit")
+  //       console.log(answer)
+  //       return JSON.parse(answer);
+  //     }else{
+  //       let url = await URL.findOne({ options: { $in: data } }); //check if the data(cutsom name) is in options field
+  //       if (url == null) {
+  //         url = await URL.findOne({ shortURL: `localhost:3000/${data}` }); //check if the data is in shortURL field
+  //       }
+  //       if (url !== null) {
+  //         await URL.findOneAndUpdate({ shortURL: data }, { $inc: { clicks: 1 } });
+  //       } else {
+  //         return "Wrong URL";
+  //       }
+  //       console.log(url);
+  //       await redisClient.set(data, JSON.stringify(url.longUrl));
+  //       return url.longUrl ;
+  //     }
+
+     
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+   
   async getUrl(data) {
     try {
-      let url = await URL.findOne({ options: { $in: data } });
-      if (url == null) {
-        url = await URL.findOne({ shortURL: `localhost:3000/${data}` });
-      }
-      if (url !== null) {
-        await URL.findOneAndUpdate({ shortURL: data }, { $inc: { clicks: 1 } });
-        return url;
-      } else {
-        return "Wrong URL";
-      }
+      // const keyExists = await redisClient.exists(data);
+      // console.log("Key Exists:", keyExists )
+  
+      // if (keyExists) {
+      //   const value = await redisClient.get(data);
+      //   console.log("Cache Hit:", value);
+      //   return JSON.parse(value);
+      // } else {
+        let url = await URL.findOne({ options: { $in: data } });
+        if (url == null) {
+          url = await URL.findOne({ shortURL: `localhost:3000/${data}` });
+        }
+  
+        if (url !== null) {
+          await URL.findOneAndUpdate({ shortURL: data }, { $inc: { clicks: 1 } });
+          await redisClient.set(data, JSON.stringify(url.longUrl));
+          console.log("Cache Miss, Added to Redis:", url.longUrl);
+          return url.longUrl;
+        } else {
+          console.log("Wrong URL");
+          return "Wrong URL";
+        }
+      // }
     } catch (error) {
-      console.log(error);
+      console.log("Error:", error);
+      throw error;
     }
   }
+  
+  
+  
+  
 
   async getFromLongURL(data) {
     try {
@@ -87,3 +138,46 @@ class UrlRepository {
 }
 
 module.exports = UrlRepository;
+
+
+
+  // async getUrl(data) {
+  //   try {
+  //     const answer = await new Promise((resolve, reject) => {
+  //       redisClient.get(data, (err, reply) => {
+  //         if (err) {
+  //           reject(err);
+  //         } else {
+  //           resolve(reply);
+  //         }
+  //       });
+  //     });
+  
+  //     if (answer) {
+  //       console.log("Cache Hit");
+  //       return JSON.parse(answer);
+  //     } else {
+  //       let url = await URL.findOne({ options: { $in: data } });
+  //       if (url == null) {
+  //         url = await URL.findOne({ shortURL: `localhost:3000/${data}` });
+  //       }
+  //       if (url !== null) {
+  //         await URL.findOneAndUpdate({ shortURL: data }, { $inc: { clicks: 1 } });
+  //       } else {
+  //         return "Wrong URL";
+  //       }
+  //       await new Promise((resolve, reject) => {
+  //         redisClient.set(data, JSON.stringify(url), (err, reply) => {
+  //           if (err) {
+  //             reject(err);
+  //           } else {
+  //             resolve(reply);
+  //           }
+  //         });
+  //       });
+  //       return url;
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
